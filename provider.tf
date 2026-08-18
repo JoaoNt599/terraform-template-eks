c706f7735518df132db331524cac62f8adb8e2bd
João Vieira <netojoao599@gmail.com>
2026-08-18 08:01:22 -0300

    feat: Identation and subnets creation

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "6.60.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
