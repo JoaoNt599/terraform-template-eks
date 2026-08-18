9eb7d29cfbdce1cdd26109926c5ad55b95a74677
João Vieira <netojoao599@gmail.com>
2026-08-18 11:11:14 -0300

    feat: private subnet and variable name

--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
 variable "cidr_block" {
   type        = string
   description = "The CIDR block for the subnet (VPC)"
+}
 
+variable "project_name" {
+  type        = string
+  description = "The name to be used for resources and tags"
 }
