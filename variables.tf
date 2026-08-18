332e3f5024fd9a8a7e11d8c182e1fa3f411f4855
João Vieira <netojoao599@gmail.com>
2026-08-18 08:18:35 -0300

    feat: Dinamic region and tags

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "cidr_block" {
-    type        = string
-    description = "The CIDR block for the subnet (VPC)"
-  
+  type        = string
+  description = "The CIDR block for the subnet (VPC)"
+
 }
