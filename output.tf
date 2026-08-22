939b95a54defe7dad279cc82255a4d1c0e4c2137
João Vieira <netojoao599@gmail.com>
2026-08-22 09:53:52 -0300

    Kubernetes provider

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "oidc" {
   value = modules.eks_cluster.oidc
 }
+
+output "ca" {
+  value = modules.eks_cluster.certificate_authority
+}
+
+output "endpoint" {
+  value = modules.eks_cluster.eks_cluster_endpoint
+}
